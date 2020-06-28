bcfa6c8d113c13a7d9007d967f7d0d947c6a9ed0
Jon Doe <kaz00@vivaldi.net>
2020-06-28 18:25:08 +0300

    Added the show_usage() function to show how to use the programm. Change the way encrypt works because of a problem with ascii values gettig negative if the were too big after adding the key. TODO: Need to change the Decrypt in similar way... probably.

--- caesar.cpp
+++ caesar.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+void show_usage()
+{
+    cout << "Usage: caesar <option> <text> <key>" <<endl;
+    cout << "Options: " <<endl;
+    cout << "\t -e,  Encrypts a string with a key." << endl;
+    cout << "\t -d,  Decrypts a string if provided a key. " << endl;
+    cout << "Key must be an int number between 1-25!" << endl;
+}
+
 // Encrypt function
 int encrypt(char text[256], int key)
 {
@@ -12,23 +21,44 @@ int encrypt(char text[256], int key)
     for ( i = 0; i < text[i] != '\0'; i++ )
     {
         ch = text[i];
-
+        // I don't really understand it but for some reason if
+        // the ascii value + the key is bigger than 127 it gets negative
+        // and cannot be displayed as a character because there are no
+        // negative ascii.
+        // Instead I temporary turn then to int and test to see if they are
+        // bigger than the int asciivalue of z then make the calculations
+        // and turn into char again.
+        // There is probably a better solution or maybe I should have 
+        // convert it them all to int in the beggining and turn them to chars
+        // at the end...
+        // Need more researching.
         if ( ch >= 'a' && ch <= 'z')
         {
-            ch = ch + key;
-            if ( ch > 'z' )
+            int tmp = int(ch) + key;
+
+            if (tmp > int('z'))
             {
-                ch = ch - 'z' + 'a' - 1;
+                tmp = tmp - int('z') + int('a') - 1;
+                ch = char(tmp);
+            }
+            else
+            {
+                ch = ch + key;
             }
             text[i] = ch;
         }
         else if (ch >= 'A' && ch <= 'Z')
         {
-            ch = ch + key;
+            int tmp = int(ch) + key;
 
-            if ( ch > 'Z')
+            if (tmp > int('Z'))
             {
-                ch = ch - 'Z' + 'A' - 1;
+                tmp = tmp - int('Z') + int('A') - 1;
+                ch = char(tmp);
+            }
+            else
+            {
+                ch = ch + key;
             }
             text[i] = ch;
         }
@@ -74,14 +104,18 @@ int decrypt(char text[256], int key)
     return 0;
 }
 
+
+// Main arguments and function call are here...
 int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
-        cout << "Usage: ceasar <option> <text> <key>" << endl;
+        show_usage();
         exit(1);
     }
     // Make the 4th argument int
+    // This throws an error when not supplied with a number
+    // need to find a way to catch it and return a better one or show_usage()
     string str = argv[3];
     std::string::size_type sz;
     int x = stoi(str, &sz);
@@ -97,7 +131,8 @@ int main(int argc, char* argv[])
     }
     else
     {
-        cout << "WTF?! is wrong?!" << endl;
+        show_usage();
+        exit(1);
     }
 }
 
