2e2840ef052d44b8b65d66d296d0c804519bf1bd
Jon Doe <kaz00@vivaldi.net>
2020-06-28 16:26:29 +0300

    Added decryption function.

--- caesar.cpp
+++ caesar.cpp
@@ -39,6 +39,41 @@ int encrypt(char text[256], int key)
     return 0;
 }
 
+// Decrypt function
+int decrypt(char text[256], int key)
+{
+    char ch;
+    int i;
+    
+    for (i = 0; i < text[i] != '\0'; i++)
+    {
+        ch = text[i];
+
+        if (ch >= 'a' && ch <= 'z')
+        {
+            ch = ch - key;
+            if (ch < 'a')
+            {
+                ch = ch + 'z' - 'a' + 1;
+            }
+            text[i] = ch;
+        }
+        else if (ch  >= 'A' && ch <= 'Z')
+        {
+            ch = ch - key;
+            if (ch < 'A')
+            {
+                ch = ch + 'Z' - 'A' + 1;
+            }
+            text[i] = ch;
+        }
+    }
+    cout << "Decrypted message: " << endl;
+    cout << text << endl;
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     if (argc < 3)
@@ -55,6 +90,10 @@ int main(int argc, char* argv[])
     if (string(argv[1]) == "-e")
     {
         encrypt(argv[2], x);
+    }
+    else if (string(argv[1]) == "-d")
+    {
+        decrypt(argv[2], x);
     }
     else
     {
